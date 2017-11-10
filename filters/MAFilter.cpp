63ea9bf027f3ff5249ac39e482c3a7ace160b76b
zcbmlijygrdwa <zcbmlijygrdwa@ymail.com>
2017-11-10 14:04:35 -0800

    document MA filter

--- filters/MAFilter.cpp
+++ filters/MAFilter.cpp
@@ -1,3 +1,38 @@
+/*============================
+This class is an implementation of a moving average filter. This is a single-input and single-output filter.
+
+The class take the raw data as the input, and returns the filtered data as the output.
+
+Input:
+dataIn: raw data input
+
+Ouput:
+filteredData: filtered data
+
+Parameters:
+None
+
+Usage:
+
+Filter maf;
+maf.init();
+
+......
+realTimeLoop
+{
+....
+filteredData = maf.spin(rawData);
+...
+}
+
+maf.reset(); //reset the filter
+
+
+Created by Zhenyu Yang on 2017/10/25
+
+==============================================*/
+
+
 class MAFilter
 {
 
@@ -7,6 +42,7 @@ class MAFilter
 		int totalSize;
 		bool ifBufferFull;
 		float sum;
+		float filteredData;
 
 	public:
 
@@ -38,14 +74,16 @@ class MAFilter
 
 			if(!ifBufferFull)
 			{
-				return sum/bufferedSize;
+				filteredData =  sum/bufferedSize;
 
 			}
 			else
 			{
 
-				return sum/totalSize;
+				filteredData =  sum/totalSize;
 			}
+
+		return filteredData;
 
 		}
 
